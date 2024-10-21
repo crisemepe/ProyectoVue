0781cebb690d6fd5f34a436ef8104098fb9700ba
Crisemepe <crisxxl@hotmail.com>
2024-10-21 22:09:25 +0200

    create new project

---
+++ ProyectoVue/src/components/HeaderComponent.vue
@@ -0,0 +1,65 @@
+
+<script>
+import logo from '@/assets/images/Logo-Borracha-Designada.png';
+
+export default {
+  name: 'HeaderComponent',
+  data() {
+    return {
+      logo,
+    };
+  },
+}
+</script>
+
+<template>
+ <!-- <v-header>
+    <v-app-bar :elevation="2" rounded>
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </template>
+
+      <v-app-bar-title>Application Bar</v-app-bar-title>
+      <v-avatar color="surface-variant"></v-avatar>
+    </v-app-bar>
+
+  </v-header>
+  -->
+
+  <v-header app>
+    <v-toolbar class="menu">
+      <!-- Menú desplegable -->
+      <v-menu offset-y>
+        <template v-slot:activator="{on}">
+          <v-btn text v-on="on">
+            Menú
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item>
+            <v-list-item-title>CARTA</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title>RESERVAS</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title>CONTACTO</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+
+      <v-toolbar-title class="d-flex justify-center">
+        <img :src="logo" alt="Logo" height="70" />
+      </v-toolbar-title>
+
+
+      <!--<v-btn icon>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>-->
+      <v-avatar color="surface-variant"></v-avatar>
+    </v-toolbar>
+  </v-header>
+
+</template>
+
---
+++ ProyectoVue/src/components/HomeComponent.vue
@@ -0,0 +1,38 @@
+<script setup>
+import HeaderComponent from "./HeaderComponent.vue";
+import {ref} from "vue";
+
+const images = [  //Variable estática
+    "https://cdn.vuetifyjs.com/images/cards/docks.jpg",
+    "https://cdn.vuetifyjs.com/images/cards/hotel.jpg",
+    "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+    ]
+var increxendo = ref(0); //Variable dinámica
+function aumento() { //función para aumentar el número de increxendo
+  increxendo.value++;
+}
+</script>
+
+<style lang="scss">
+@import '../styles/style.scss';
+</style>
+
+<template>
+  <HeaderComponent> </HeaderComponent>
+
+  <v-container class="contenedor">
+    <v-carousel>
+      <v-carousel-item v-for="image in images":key="image" :src="image" cover
+      ></v-carousel-item>
+    </v-carousel>
+    <v-row>
+      <v-col>
+        <v-label>{{increxendo}}</v-label>
+        <v-btn @click="aumento()">Bunito</v-btn>
+      </v-col>
+    </v-row>
+
+  </v-container>
+
+  <FooterComponent> </FooterComponent>
+</template>
