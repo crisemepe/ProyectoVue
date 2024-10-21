be5abf5739677dcf3823c4b812023173a8818fa4
Crisemepe <crisxxl@hotmail.com>
2024-10-22 00:10:13 +0200

    new page

--- ProyectoVue/src/components/HeaderComponent.vue
+++ ProyectoVue/src/components/HeaderComponent.vue
@@ -1,19 +1,24 @@
+<script setup>
+import logo from "@/assets/images/Logo-LBD.png";
 
-<script>
-import logo from '@/assets/images/Logo-Borracha-Designada.png';
-
-export default {
-  name: 'HeaderComponent',
-  data() {
-    return {
-      logo,
-    };
+const puntosmenu=[
+  {
+    "icon": "mdi-book",
+    "text": "CARTA"
   },
-}
+  {
+    "icon": "mdi-book-open",
+    "text": "RESERVAS"
+  },
+  {
+    "icon": "mdi-phone",
+    "text": "CONTACTO"
+  }
+];
 </script>
 
 <template>
- <!-- <v-header>
+  <!-- <v-header>
     <v-app-bar :elevation="2" rounded>
       <template v-slot:prepend>
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
@@ -29,37 +34,27 @@ export default {
   <v-header app>
     <v-toolbar class="menu">
       <!-- Menú desplegable -->
-      <v-menu offset-y>
-        <template v-slot:activator="{on}">
-          <v-btn text v-on="on">
-            Menú
-          </v-btn>
+      <v-menu transition="slide-y-transition">
+        <template v-slot:activator="{ props }">
+          <v-btn color="primary" v-bind="props"> Menú </v-btn>
         </template>
         <v-list>
-          <v-list-item>
-            <v-list-item-title>CARTA</v-list-item-title>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-title>RESERVAS</v-list-item-title>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-title>CONTACTO</v-list-item-title>
+          <v-list-item v-for="(item, i) in puntosmenu" :key="i">
+            <v-list-item-title>{{ item.text }}</v-list-item-title>
           </v-list-item>
         </v-list>
       </v-menu>
 
-
-      <v-toolbar-title class="d-flex justify-center">
-        <img :src="logo" alt="Logo" height="70" />
+      <v-toolbar-title class="d-flex justify-center mt-8 mb-6">
+        <img :src="logo" alt="Logo" height="60" />
       </v-toolbar-title>
-
 
       <!--<v-btn icon>
         <v-icon>mdi-account</v-icon>
       </v-btn>-->
-      <v-avatar color="surface-variant"></v-avatar>
+      <v-avatar color="white" class="mr-9">
+      <v-icon icon="mdi-account-circle"></v-icon>
+    </v-avatar>
     </v-toolbar>
   </v-header>
-
 </template>
-
--- ProyectoVue/src/components/HomeComponent.vue
+++ ProyectoVue/src/components/HomeComponent.vue
@@ -2,6 +2,12 @@
 import HeaderComponent from "./HeaderComponent.vue";
 import {ref} from "vue";
 
+const menuItems = ref([
+  { icon: 'mdi-book', text: 'CARTA' },
+  { icon: 'mdi-book-open', text: 'RESERVAS' },
+  { icon: 'mdi-phone', text: 'CONTACTO' },
+]);
+
 const images = [  //Variable estática
     "https://cdn.vuetifyjs.com/images/cards/docks.jpg",
     "https://cdn.vuetifyjs.com/images/cards/hotel.jpg",
@@ -25,14 +31,32 @@ function aumento() { //función para aumentar el número de increxendo
       <v-carousel-item v-for="image in images":key="image" :src="image" cover
       ></v-carousel-item>
     </v-carousel>
-    <v-row>
+
+   <!-- <v-row>
       <v-col>
         <v-label>{{increxendo}}</v-label>
         <v-btn @click="aumento()">Bunito</v-btn>
       </v-col>
-    </v-row>
-
+    </v-row>-->
   </v-container>
+
+  <v-container fluid class="text-center pa-4">
+    <v-row justify="center">
+      <v-col
+        v-for="(item, index) in menuItems"
+        :key="index"
+        cols="4"
+        class="d-flex flex-column align-center"
+      >
+        <v-btn icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-btn>
+        <span>{{ item.text }}</span>
+      </v-col>
+    </v-row>
+  </v-container>
+
+
 
   <FooterComponent> </FooterComponent>
 </template>
